e421a290f36c0873625519280bfbe8243df39726
7cgrmty89c-star <7cgrmty89c@privaterelay.appleid.com>
2026-08-14 02:28:13 +0200

    Create Main.py
    initial business pulse check

---
+++ Main.py
@@ -0,0 +1,6 @@
+# asystent analizujacy profil tatuażysty i podpowiadajacy jak zdowbywc klientów
+print("tattoo bizness asystent")
+print("witaj w programie")
+klienci = input("ilu klientów miałeś w tym miesiącu")
+if int(klienci) < 10 :
+	print("zacznij kampanie")
